37cc81fdc05b3bce797e75af33b7e6cbc6b2513d
bmloiank <hajupark110324@gmail.com>
2026-03-11 22:50:55 +0900

    commit메세지 사용방법 추가

--- main.py
+++ main.py
@@ -17,12 +17,6 @@ git config --global core.quotepath false
 $ git init 
 >> Initialized empty Git repository in C:/Users/user/Desktop/python/.git/
 
-<git 흐름>
-작업한 파일               ------ add ------> 대기상태 -> 저장
-untracked(저장을한번도안한파일)
-modify(저장햇으나 수정한 파일)
-
-
 2. git 현재상태보기
 $ git status
 - 빨간색 : 수정한 파일
@@ -53,14 +47,12 @@ git remote -v
 
 8. 업로드 git push -u origin main
 
-'''
 
+<git 흐름>
+파일을 수정하면 -> stage 상태로 올려놓기 -> 저장
+- untracked(저장을한번도안한파일)
+- modify(저장햇으나 수정한 파일)
 
-
-# git
-
-# 파일을 수정하면 -> stage 상태로 올려놓기 -> 저장
-#                (대기)
 # 예제 opencv폴더 안에 cv01.py
 # git add opencv/cv01.py
 # git status
@@ -72,16 +64,30 @@ git remote -v
 # git push -u origin main
 
 
+#  임시저장하기 add
+#  임시저장 취소하기 reset
+# $ git reset 파일
+
+# 전체 임시저장하기 git add . git add -A git add --all
+# data dist build
 
 
 
 
 
+# git commit -m "간단한 메세지"
+
+# # 누구 언제 어디서 왜 자세하게 적어야한다.
+# git commit # 본격적인 메세지
 
 
+# VIM 리눅스(서버 컴퓨터) 에디터
+# :q
+# i insert모드 수정가능하다.
+# esc + :q : 종료
+# esc + :q! : 강제종료
 
-
-
-
-
-
+# :wq : 저장하고 종료
+# :wq! : 강제로 저장하고 종료
+# 키보드로 명령어를 쳐서 명령어는 방법을 command line 명령어
+'''
